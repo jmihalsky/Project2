3e339264c9fdb1ba970942d6c929b3bc8f9b1855
casekercan <casekercan@gmail.com>
2018-12-05 21:14:06 -0800

    index.handlebars populating Post fields

--- routes/slist_routes.js
+++ routes/slist_routes.js
@@ -4,14 +4,14 @@ var router = express.Router();
 var slist = require("../models/slist.js");
 
 router.get("/", function (req, res) {
-    res.render("index");
-    // slist.all(function (sposts) {
-    //     var slistposts = {
-    //         sposts: sposts
-    //     };
-    //     console.log(slistposts);
-    //     res.render("index", slistposts);
-    // });
+    // res.render("index");
+    slist.all(function (sposts) {
+        var slistposts = {
+            sposts: sposts
+        };
+        console.log(slistposts);
+        res.render("index", slistposts);
+    });
 });
 
 module.exports = router;
