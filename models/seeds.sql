a4b169edfc570f8b699b4552a1c4703fb9959e74
thirteen <41817587+maguirre13@users.noreply.github.com>
2018-12-08 14:13:32 -0800

    clean up code

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,4 +1,3 @@
-
 INSERT INTO Usr (username, pword, email, av_image) VALUES ("casekercan", "logmein", "casekercan@gmail.com", "/assets/img/profile_img/caroline.png");
 
 INSERT INTO Posts (userID, LocationName, LocAddr, City, State, Zip, PostText, PostRating, post_image) VALUES (1 , "The Lone Toilet", "5400 Sears Point Rd", "Sonoma", "CA", 95476, "In the middle of nowhere in Sonoma, there is this outhouse with a very clean toilet. It is not near any kind of facility, camping ground, or anything else.", 4 , "/assets/img/post_img/lone.jpg");
