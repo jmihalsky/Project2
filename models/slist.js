cb9c9e3f804ab15887452b03eeaa21cc0701fff8
casekercan <casekercan@gmail.com>
2018-12-08 11:43:45 -0800

    updated handlebars layouts, and updated index.js in public folder

--- models/slist.js
+++ models/slist.js
@@ -1,13 +1,13 @@
 var orm = require("../config/orm.js");
 
 var slist = {
-    all: function(qryres){
-        orm.all_posts(function(res){
+    all: function (qryres) {
+        orm.all_posts(function (res) {
             qryres(res);
         });
     },
-    city: function(qryres){
-        orm.city_search(function(res){
+    city: function (qryres) {
+        orm.city_search(function (res) {
             qryres(res);
         });
     }
