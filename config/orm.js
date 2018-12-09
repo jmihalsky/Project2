d18ca68a6bcfbdfb8ac3f9f4a44cc6428cd86e4d
casekercan <casekercan@gmail.com>
2018-12-09 14:37:30 -0800

    corrected files

--- config/orm.js
+++ config/orm.js
@@ -1,39 +1,39 @@
 var connection = require("./connection.js");
 
 var orm = {
-  all_posts: function(qryres) {
+  all_posts: function (qryres) {
     var qrystrg =
       "select Posts.*, Usr.username from Posts inner join Usr on Posts.UserID = Usr.UserID";
-    connection.query(qrystrg, function(err, res) {
+    connection.query(qrystrg, function (err, res) {
       if (err) throw err;
 
       qryres(res);
     });
   },
-  city_search: function(schcity, qryres) {
+  city_search: function (schcity, qryres) {
     var qrystrg =
       "select Posts.*, Usr.username from Posts inner join Usr on Posts.UserID = Usr.UserID where Posts.City like '%" +
       schcity +
       "%'";
-    connection.query(qrystrg, function(err, res) {
-      if (eff) throw err;
+    connection.query(qrystrg, function (err, res) {
+      if (err) throw err;
       qryres(res);
     });
   },
-  zip_search: function(schzip, qryres) {
+  zip_search: function (schzip, qryres) {
     var qrystrg =
       "select Posts.*, Usr.username from Posts inner join Usr on Posts.UserID = Usr.UserID where Posts.Zip = " +
       schzip;
-    connection.query(qrystrg, function(err, res) {
-      if (eff) throw err;
+    connection.query(qrystrg, function (err, res) {
+      if (err) throw err;
       qryres(res);
     });
   },
-  post_info: function(post_id, qryres) {
+  post_info: function (post_id, qryres) {
     var qrystrg =
       "select Posts.*, Usr.username from Posts inner join Usr on Posts.UserID = Usr.UserID where Posts.PostID = " +
       post_id;
-    connection.query(qrystrg, function(err, res) {
+    connection.query(qrystrg, function (err, res) {
       if (err) throw err;
       qryres(res);
     });
