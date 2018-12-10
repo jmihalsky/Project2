b05ff0385c8751e74b97258acfe4c5a97a9ef601
jmihalsky <jmihalsky@gmail.com>
2018-12-09 21:19:57 -0800

    initial commit

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -61,3 +61,73 @@ function checkSearch(zipState, cityState, searchParam) {
     console.log("nothing worked");
   }
 };
+
+$(function() {
+  $(".delcomment").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/comments/" + id, {
+      type: "DELETE"
+    }).then(function() {
+      console.log("deleted id ", id);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $("input[name=rating]").on("click", function() {
+    console.log("click");
+    $("#ratings").val($(this).attr("value"));
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newComment = {
+      CommentRating: $("#ratings")
+        .val()
+        .trim(),
+      CommentText: $("#com")
+        .val()
+        .trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/comments", {
+      type: "POST",
+      data: newComment
+    }).then(function() {
+      console.log("created new comment");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(".update-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var updatedComment = {
+      CommentRating: $("#ratings")
+        .val()
+        .trim(),
+      CommentText: $("#com")
+        .val()
+        .trim()
+    };
+
+    var id = $(this).data("id");
+
+    // Send the POST request.
+    $.ajax("/api/comments/" + id, {
+      type: "PUT",
+      data: updatedComment
+    }).then(function() {
+      console.log("updated comment");
+      // Reload the page to get the updated list
+      location.assign("/");
+    });
+  });
+});
