140c0acd788c80022997b60de00636e3246b2e40
casekercan <casekercan@gmail.com>
2018-12-09 20:43:15 -0800

    search form verification

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -17,20 +17,69 @@ $(".login").on("click", function (event) {
 //Search button
 $("#search").submit(function (event) {
   event.preventDefault();
+  var zipState = false;
+  var cityState = false;
+  var searchParam = $("#search_text").val();
 
-  if ($("#zip").is(':checked')) {
-    var zip = $("#search_text").val();
-    var url = ("/zip_search/" + zip);
-    window.location = url;
+  if ($("#zip").is(":checked")) {
+    zipState = true;
+  };
 
-  } else if ($("#city").is(':checked')) {
-    var city = $("#search_text").val();
-    var url = ("/city_search/" + city);
-    window.location = url;
+  if ($("#city").is(":checked")) {
+    cityState = true;
+  };
 
+  if (searchParam == "") {
+    console.log("empty");
+    alert("Please insert a City/Zip Code to search.")
+    return;
   } else {
-    alert("Please check Zip OR City box to search");
+    checkSearch(zipState, cityState, searchParam);
   };
 
 });
 
+function checkSearch(zipState, cityState, searchParam) {
+  console.log("zipState = " + zipState);
+  console.log("cityState = " + cityState);
+  console.log("searchParam = " + searchParam);
+
+  if ((zipState === true) && (cityState === true)) {
+    alert("Please only search by the Zip Code OR City.");
+    console.log("both true");
+  } else if ((zipState === false) && (cityState === false)) {
+    alert("Please check either Zip Code or City to search.");
+    console.log("both false");
+
+  } else if (zipState === true) {
+    var url = ("/zip_search/" + searchParam);
+    window.location = url;
+  } else if (cityState === true) {
+    var url = ("/city_search/" + searchParam);
+    window.location = url;
+  } else {
+    console.log("nothing worked");
+  }
+
+
+  // if ($("#zip").is(':checked')) {
+  //   var zip = $("#search_text").val();
+  //   var url = ("/zip_search/" + zip);
+  //   window.location = url;
+  //   return;
+
+  // } else if ($("#city").is(':checked')) {
+  //   var city = $("#search_text").val();
+  //   var url = ("/city_search/" + city);
+  //   window.location = url;
+  //   return;
+
+  // } else if (($("#city").is(':checked')) && ($("#city").is(':checked'))) {
+  //   alert("Please only search by the Zip Code OR City.")
+  //   return;
+
+  // } else {
+  //   alert("Please check Zip OR City box to search");
+  // };
+
+};
