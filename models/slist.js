a264427fb4277c95f5bc89acdd0694aa5042c4d6
casekercan <casekercan@gmail.com>
2018-12-09 16:01:55 -0800

    updated search/city slist.js functions

--- models/slist.js
+++ models/slist.js
@@ -1,23 +1,23 @@
 var orm = require("../config/orm.js");
 
 var slist = {
-  all: function(qryres) {
-    orm.all_posts(function(res) {
+  all: function (qryres) {
+    orm.all_posts(function (res) {
       qryres(res);
     });
   },
-  city: function(qryres) {
-    orm.city_search(function(res) {
+  city: function (schcity, qryres) {
+    orm.city_search(schcity, function (res) {
       qryres(res);
     });
   },
-  zip: function(qryres) {
-    orm.zip_search(function(res) {
+  zip: function (schzip, qryres) {
+    orm.zip_search(schzip, function (res) {
       qryres(res);
     });
   },
-  posts: function(post_id, qryres) {
-    orm.post_info(post_id, function(res) {
+  posts: function (post_id, qryres) {
+    orm.post_info(post_id, function (res) {
       qryres(res);
     });
   }
