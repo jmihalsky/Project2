63638ede96a3191f889428efb631a73ad85e7da2
jmihalsky <jmihalsky@gmail.com>
2018-12-12 23:05:16 -0800

    initial commit for branch

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var session = require("express-session");
 var PORT = process.env.PORT || 3000;
 var app = express();
 var fileUpload = require('express-fileupload');
