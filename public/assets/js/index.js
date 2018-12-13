b01d1414b4dbc15ee5fef2cde18930d44b972ab3
David <40313600+dlara319@users.noreply.github.com>
2018-12-12 20:48:18 -0800

    workign on adding database to maps

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -313,8 +313,26 @@ function initMap() {
     });
   } else {
     handleLocationERROR(false, infoWindow, map.getCenter());
-  }
+  };
+
+var script = document.createElement("script");
+
+script.src = "slist";
+document.getElementsByTagName("head")[0].appendChild(script);
+
 };
+
+window.eqfeed_callback = function(results) {
+  for (var i = 0; i < results.features.length; i++) {
+    var coords = results.features[i].geometry.coordinates;
+    var latLng = new google.maps.LatLng(coords[1],coords[0]);
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+  }
+}
+
 
 function handleLocationERROR(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
