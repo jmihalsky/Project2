dbaf48b2ed390f5ba251131f85050277351f4e45
casekercan <casekercan@gmail.com>
2018-12-12 21:55:58 -0800

    updated env

--- routes/slist_routes.js
+++ routes/slist_routes.js
@@ -7,7 +7,7 @@ var path = require("path");
 //var S3_BUCKET = process.env.S3_BUCKET;
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 AWS.config.region = 'eu-east-1';
